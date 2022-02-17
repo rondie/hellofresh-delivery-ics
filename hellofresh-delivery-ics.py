27edf9490ea334881bea9295cbf8ca4c2fd69c0b
Rondie <github@rondie.nl>
2022-02-17 20:21:14 +0100

    Create hellofresh-delivery-ics.py
    working script, perhaps tweaks to be made to allow other countries than NL (assuming HF does things the same everywhere they operate)

---
+++ hellofresh-delivery-ics.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+from datetime import datetime
+from datetime import timedelta
+import ics import Calendar, Event
+
+username = '<email>'
+password = '<password>'
+url = 'https://www.hellofresh.nl'
+icsfile = '/tmp/hellofresh-delivery.ics'
+addminutes = 15
+country = 'nl'
+locale = 'nl-NL'
+
+#login page
+loginurl = url + '/gw/login'
+logindata = { 'username': username, 'password': password }
+logindataJson = json.dumps(logindata, indent=4)
+
+#token page
+tokenpage = requests.post(loginurl, data = logindataJson)
+tokenJson = json.loads(tokenpage.text)
+tokenType = tokenJson['token_type']
+token = tokenJson['access_token']
+headers = {
+            'Authorization': tokenType + ' ' + token
+        }
+params = dict()
+params['country'] = country
+params['locale'] = locale
+
+#deliveries overview
+deliverylinkurl = url + '/gw/api/customers/me/deliveries'
+deliverylinkpage = requests.get(deliverylinkurl, headers=headers, params=params)
+deliverylinkJson = json.loads(deliverylinkpage.text)
+
+#delivery page with start time
+deliveryurl = deliverylinkJson['items'][0]['tracking']['tracking_link']
+deliverystartstring = deliverylinkJson['items'][0]['tracking']['estimated_delivery_time']
+deliverystart = datetime.strptime(deliverystartstring, "%Y-%m-%dT%H:%M:%S+0000")
+#calculate the end time
+deliveryend = deliverystart + timedelta(minutes=addminutes)
+
+#ics event
+from ics import Calendar, Event
+c = Calendar()
+e = Event()
+e.name = 'Hellofresh'
+e.begin = deliverystart
+e.end = deliveryend
+e.description = deliveryurl
+c.events.add(e)
+c.events
+with open(icsfile, 'w') as my_file:
+    my_file.writelines(c)
